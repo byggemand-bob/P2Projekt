a084f780bd2f4cbc5ee3b273da0161189062f3c6
byggemand-bob <45236808+byggemand-bob@users.noreply.github.com>
2019-03-29 12:49:03 +0100

    Revert "clean"
    This reverts commit 61a4e5ab705a84c42211e532e7aed43e96d92500.

---
+++ Poker_Game/Poker_Game/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_Game {
+    class Player {
+        public int Stack { get; set; }
+        public bool IsSmallBlind { get; set; }
+        public bool IsBigBlind { get; set; }
+        public bool HasFolded { get; set; }
+        public List<Card> Cards = new List<Card>();
+    }
+}
