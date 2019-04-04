8975e614d7e68618151a9c4e24d9d76c59bc9de4
byggemand-bob <45236808+byggemand-bob@users.noreply.github.com>
2019-04-04 14:09:24 +0200

    Update WinConditions.cs

--- Poker_Game/Poker_Game/Game/WinConditions.cs
+++ Poker_Game/Poker_Game/Game/WinConditions.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Poker_Game {
     class WinConditions {
-
+        private static bool isFlush(PokerHand h) {//taget fra PokerLogic
+            if (h[0].suit == h[1].suit &&
+                h[1].suit == h[2].suit &&
+                h[2].suit == h[3].suit &&
+                h[3].suit == h[4].suit)
+                return true;
+            return false;
+        }
 
 
     }
