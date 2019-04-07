50a38cb9d7dd62b3809302a176a957c23810cd47
Magnus Mølgaard Lund <themlund98@gmail.com>
2019-04-07 23:18:28 +0200

    Update AI.cs

--- Poker_Game/Poker_Game/AI/AI.cs
+++ Poker_Game/Poker_Game/AI/AI.cs
@@ -6,5 +6,31 @@ using System.Threading.Tasks;
 
 namespace Poker_Game.AI {
     class AI {
+        public Player Player { get; set; }
+        public List<Hand> Hands { get; set; }
+        public int Pot { get; set; }
+        public Settings Settings { get; set; }
+        
+        public AI(Player player, List<Hand> hands, Settings settings) {
+            Player = player;
+            Hands = hands;
+            Settings = settings;
+        }
+
+
+        public void MakeDecision() {
+            
+        }
+
+        // Spørg Mikkel om decimal er den bedste datatype
+        private decimal CalcBinomial(int currentBet, int pot) {
+            int possibleBet = currentBet + 2 * Settings.BlindSize;
+
+                
+
+
+            
+
+        }
     }
 }
