f3919606a546ae3b7c88100c78fe980deb82a3a6
Magnus Mølgaard Lund <themlund98@gmail.com>
2019-04-07 19:17:39 +0200

    Update Card_test.cs

--- Poker_Game/Poker_Game/Card_test.cs
+++ Poker_Game/Poker_Game/Card_test.cs
@@ -10,24 +10,19 @@ namespace Poker_Game {
             Console.WriteLine("Hello World!");
             List<Player> players = new List<Player>() { new Player(0, 1000), new Player(1, 1000) };
             Hand h = new Hand(players);
+            int NumberOfCards = 9;
 
 
-            for(int i = 0; i < 52; i++) {
+
+            for(int i = 0; i < NumberOfCards; i++) {
                 h.Deck.Add(new Card(h.Deck));
             }
             h.Deck.Sort();
-            for(int j =0; j < 52; j++)
+            for(int j =0; j < NumberOfCards; j++)
             {
                 Console.WriteLine(h.Deck[j].Rank + " " + h.Deck[j].Suit);
             }
             Console.WriteLine("Done!");
-            //for (int j = 0; j < 52; j++)
-            //{
-            //    Card testkort = Card.MakeCard(j);
-
-            //    Console.WriteLine("Kort er " + testkort.Suit + " " + testkort.Rank);
-            //}
-            //Console.WriteLine("Done!");
             Console.ReadKey();
         }
     }
