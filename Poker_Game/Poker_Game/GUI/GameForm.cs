8d6b46d77fd1c180fb6d1f495c7725002de4688c
Jannik Sommer <jannik.sommer@sommerhouse.net>
2019-04-16 23:01:20 +0200

    Update GameForm.cs

--- Poker_Game/Poker_Game/GUI/GameForm.cs
+++ Poker_Game/Poker_Game/GUI/GameForm.cs
@@ -12,6 +12,8 @@ namespace Poker_Game {
         private readonly List<PictureBox> PictureBoxes = new List<PictureBox>();
         private const bool DiagnosticsMode = true;
 
+         // TODO: Make prettier and show who wins and how much at showdown
+
         #region Initialization
 
         public GameForm(string inputPlayerName, int inputStackSize, int inputBlindSize, int blindIncrease, bool blindIsRoundBased) { //Think about making Settings in settingsform and has it as a parameter. 
