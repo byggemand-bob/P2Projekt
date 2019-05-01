31fe54ff7ccdc4ad5a42f40c351de7911b025aed
Magnus Mølgaard Lund <mlund18@student.aau.dk>
2019-05-01 14:07:49 +0200

    Save and load VPIP data

---
+++ Poker_Game/Poker_Game/Game/VPIPData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_Game.Game {
+    class VPIPData {
+        public string PlayerName { get; }
+        public int NumberCalls { get; set; }
+        public int NumberOfHands { get; set; }
+        public int NumberRaises { get; set; }
+
+        public VPIPData(string playerName, int numberOfHands, int numberCalls, int numberRaises) {
+            PlayerName = playerName;
+            NumberCalls = numberCalls;
+            NumberOfHands = numberOfHands;
+            NumberRaises = numberRaises;
+        }
+    }
+}
---
+++ Poker_Game/Poker_Game/Game/VPIPReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_Game.Game {
+    class VPIPReader {
+        private readonly string _filePath;
+
+        public VPIPReader(string playerName) {
+            _filePath = CreateFilePath(playerName);
+        }
+
+        private string CreateFilePath(string playerName) {
+            return System.Windows.Forms.Application.StartupPath + "\\VPIPData\\" + playerName + ".vpip";
+        }
+
+        public VPIPData ReaData() {
+            StreamReader sr = new StreamReader(_filePath);
+            return new VPIPData(sr.ReadLine(),
+                Int32.Parse(sr.ReadLine()), 
+                Int32.Parse(sr.ReadLine()), 
+                Int32.Parse(sr.ReadLine()));
+            sr.Close();
+        }
+    }
+}
