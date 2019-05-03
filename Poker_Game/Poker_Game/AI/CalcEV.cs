e4484c2069117e647bb70b7a6afb6e4836107ed5
Per Frost <48987550+PerFrost@users.noreply.github.com>
2019-05-03 09:34:40 +0200

    Update

--- Poker_Game/Poker_Game/AI/CalcEV.cs
+++ Poker_Game/Poker_Game/AI/CalcEV.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Poker_Game.Game;
 
 namespace Poker_Game.AI
 {
-    class IsParetoOptimal
+    class CalcEV
     {
+        public Player players { get; set; }
+
+        public void CalcEv(Player player) {
+            players = player;
+        }
     }
 }
--- Poker_game/Poker_game/Preflop.cs
+++ Poker_game/Poker_game/Preflop.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Poker_Game.AI
+namespace Poker_Game
 {
     class Preflop
     {
