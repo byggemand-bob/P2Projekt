d98e35ae5cafd50b52af4c53a31ed8d7b64b36b9
Per Frost <48987550+PerFrost@users.noreply.github.com>
2019-05-08 05:08:42 +0200

    AI Update

--- Poker_game/Poker_game/AI/CalcProbability.cs
+++ Poker_game/Poker_game/AI/CalcProbability.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Poker_Game.Game;
 
 namespace Poker_Game.AI
 {
     class CalcProbability
     {
+        public Player Player { get; set; }
+        public List<Card> Street { get; set; }
+        public PlayerCardsInHand PlayerCardsInHands { get; set; }
+        public int NumberOfOuts { get; set; }
+        public List<Card> flushCards { get; set; }
+
+
+        public void calcProbability(Player player, PlayerCardsInHand cardsinhands, int numberOfOuts, List<Card> flushcards) {
+            Player = player;
+            PlayerCardsInHands = cardsinhands;
+        }
+
+        private double CalculateProbability(Player player) {
+
+            double unseenCards = 52 - Street.Count + player.Cards.Count;
+            double cardOdds = unseenCards / NumberOfOuts;
+
+            return cardOdds;
+        }
     }
 }
+
+
+
+
+
