bc1d7ed31d261feb1173e9e20b9a004c94d939b6
KristianWP <43957853+KristianWP@users.noreply.github.com>
2019-05-12 21:29:36 +0200

    added test

--- Poker_Game/UnitTest/FastWinCalcTest.cs
+++ Poker_Game/UnitTest/FastWinCalcTest.cs
@@ -156,5 +156,41 @@ namespace UnitTest
             // Assert
             Assert.AreEqual(1, actual);
         }
+
+        [TestMethod]
+        public void FastWinCalcTest5()
+        {
+            // Arrange
+            List<Card> Player1Cards = new List<Card>();
+            List<Card> Player2Cards = new List<Card>();
+
+            Card tableCard1 = new Card(Suit.Clubs, (Rank)3);
+            Card tableCard2 = new Card(Suit.Clubs, (Rank)2);
+            Card tableCard3 = new Card(Suit.Clubs, Rank.Ace);
+            Card tableCard4 = new Card(Suit.Spades, (Rank)10);
+            Card tableCard5 = new Card(Suit.Spades, Rank.Queen);
+
+            Player1Cards.Add(new Card(Suit.Diamonds, Rank.King));
+            Player1Cards.Add(new Card(Suit.Diamonds, Rank.Jack));
+            Player1Cards.Add(tableCard1);
+            Player1Cards.Add(tableCard2);
+            Player1Cards.Add(tableCard3);
+            Player1Cards.Add(tableCard4);
+            Player1Cards.Add(tableCard5);
+
+            Player2Cards.Add(new Card(Suit.Clubs, (Rank)4));
+            Player2Cards.Add(new Card(Suit.Clubs, (Rank)5));
+            Player2Cards.Add(tableCard1);
+            Player2Cards.Add(tableCard2);
+            Player2Cards.Add(tableCard3);
+            Player2Cards.Add(tableCard4);
+            Player2Cards.Add(tableCard5);
+
+            // Act
+            int actual = winCalc.WhoWins(Player1Cards, Player2Cards);
+
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
     }
 }
