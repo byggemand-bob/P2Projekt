53cdf79846a1b40bcd3440c02d1c6de1fe73d4b5
Per Frost <48987550+PerFrost@users.noreply.github.com>
2019-05-16 09:57:28 +0200

    Update

---
+++ Poker_game/Poker_game/AI/Opponent/EVComparison.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Poker_Game.Game;
+
+namespace Poker_Game.AI.Opponent
+{
+    class EVComparison {
+
+        public Player Player { get; set; }
+        public Settings Settings { get; set; }
+
+        public EVComparison(Player player, Settings settings) {
+            Player = player;
+            Settings = settings;
+        }
+
+        public double EVCompare(Player player, Settings settings, List<Card> playerCardHands, List<Card> AICardHands) { 
+
+            EVCalculator ec = new EVCalculator(player, settings);
+
+            var playerEv = ec.CalculateEv(List<Card> playerCardHands);
+            var AIev = ec.CalculateEv(List<Card> AICardHands);
+
+            if (playerEv < AIev) {
+                player.Action = PlayerAction.Fold;
+            }
+
+            else if (playerEv > AIev) {
+                if (playerEv > 1) {
+                    player.Action = PlayerAction.Raise;
+                }
+
+                if (playerEv > 0 && playerEv < 1) {
+                    player.Action = PlayerAction.Call;
+                }
+            }
+        }
+    }
+}
