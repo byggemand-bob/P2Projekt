5a6b475e4c7c0d39ac836f935865caaf945b6235
byggemand-bob <45236808+byggemand-bob@users.noreply.github.com>
2019-05-21 11:58:20 +0200

    Update WinCalcVsWinCalc.cs

--- Poker_Game/Poker_Game/WinCalcVsWinCalc.cs
+++ Poker_Game/Poker_Game/WinCalcVsWinCalc.cs
@@ -11,14 +11,15 @@ namespace Poker_Game
     {
         FastWinCalc WinCalc1 = new FastWinCalc();
         WinConditions winCalc2 = new WinConditions();
-        List<Card> CardsInPlay, Street;
+        List<Card> player1Cards, player2Cards, CardsInPlay, Street;
         Card NewCard;
         List<Player> players = new List<Player>() { new Player(0, 1000), new Player(1, 1000) };
-
         private int CheckWinner() {
             int resultWinCalc1;
             int resultWinCalc2;
             Player resultWinCalc2Player;
+            players[0].Cards.Concat(player1Cards).ToList();
+            players[1].Cards.Concat(player2Cards).ToList();
             players[0].Cards.Concat(Street).ToList();
             players[1].Cards.Concat(Street).ToList();
             players[0].Score = winCalc2.Evaluate(players[0].Cards);
@@ -39,12 +40,15 @@ namespace Poker_Game
                 }
                 if (resultWinCalc1 != resultWinCalc2) {
                     PrintPlayers();
+                    Console.ReadKey();
+                    return 1;
 
                 }
             }
         }
         private void PrintPlayers() {
-            Console.WriteLine("player1 hand: " + players[0].Cards[0].Suit + players[0].Cards[0].Rank + players[0].Cards[0].Suit + players[0].Cards[0].Rank + players[0].Cards[0].Suit + players[0].Cards[0].Rank + players[0].Cards[0].Suit + players[0].Cards[0].Rank + players[0].Cards[0].Suit + players[0].Cards[0].Rank + players[0].Cards[0].Suit + players[0].Cards[0].Rank + players[0].Cards[0].Suit + players[0].Cards[0].Rank);
+            Console.WriteLine("player1 hand: " + player1Cards[0].Suit + player1Cards[0].Rank + " " + player1Cards[1].Suit + player1Cards[1].Rank);
+            Console.WriteLine("player1 hand: " + player2Cards[0].Suit + player2Cards[0].Rank + " " + player2Cards[1].Suit + player2Cards[1].Rank);
         }
         private void NewCards()
         {
@@ -58,14 +62,14 @@ namespace Poker_Game
             {
                 NewCard = new Card(CardsInPlay);
                 CardsInPlay.Add(NewCard);
-                players[0].Cards.Add(NewCard);
+                player1Cards.Add(NewCard);
             }
 
             for (int x = 0; x < 2; x++)
             {
                 NewCard = new Card(CardsInPlay);
                 CardsInPlay.Add(NewCard);
-                players[2].Cards.Add(NewCard);
+                player2Cards.Add(NewCard);
             }
             
 
